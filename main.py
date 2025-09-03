9ba3bd1004d7e5fec7a6d9e8feebeb0c9531043d
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-09-03 09:11:25 +0300

    add get health and all phones and Characteristis model class

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 from starlette.responses import JSONResponse
 from pydantic import BaseModel
 
-from starlette.requests import Request
-
 app = FastAPI()
+
+class Characteristics(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+class phones(BaseModel):
+    identifier: str
+    brand: str
+    model : str
+    characteristics: Characteristics
 
 @app.get("/ping")
 def root():
     return JSONResponse(content={"message": "pong"}, status_code=200)
+
+@app.get("/health")
+def health():
+    return JSONResponse(content={"status": "ok"}, status_code=200)
