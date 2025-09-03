b4dcb1ed1235ecb870d3880e3be03027116b7b32
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-09-03 09:38:13 +0300

    refactor : arrange all files strucuture and name

--- main.py
+++ main.py
@@ -9,33 +9,29 @@ class Characteristics(BaseModel):
     ram_memory: int
     rom_memory: int
 
-class phones(BaseModel):
+class Phones(BaseModel):
     identifier: str
     brand: str
     model : str
     characteristics: Characteristics
-phones_mempory :List[phones] = []
-
-@app.get("/ping")
-def root():
-    return JSONResponse(content={"message": "pong"}, status_code=200)
+phones_memory :List[Phones] = []
 
 @app.get("/health")
 def health():
     return JSONResponse(content={"status": "ok"}, status_code=200)
 
 @app.post("/phones")
-def create_phone(list_phones: phones):
-    phones_mempory.extend(list_phones)
-    return phones_mempory
+def create_phone(list_phones: List[Phones]):
+    phones_memory.extend(list_phones)
+    return phones_memory
 
 @app.get("/phones")
 def get_phones():
-    return phones_mempory
+    return phones_memory
 
 @app.get("/phones/{id}")
 def get_phone_by_id(identifier: str):
-    for phone in phones_mempory:
+    for phone in phones_memory:
         if phone.identifier == identifier:
             return phone
     return JSONResponse(content={"message": "id does not exist or not found"}, status_code=404)
@@ -43,8 +39,8 @@ def get_phone_by_id(identifier: str):
 
 @app.put("/phones/{id}/characteristics")
 def update_characteristics(identifier: str, new_characteristics: Characteristics):
-    for i, phone in enumerate(phones_mempory):
+    for i, phone in enumerate(phones_memory):
         if phone.identifier == identifier:
-            phones_mempory[i].characteristics = new_characteristics
-            return {"message": "characteristics updated", "phones": phones_mempory[i].characteristics}
+            phones_memory[i].characteristics = new_characteristics
+            return {"message": "characteristics updated" , "phones": phones_memory[i] + phones_memory[i].characteristics}
     return JSONResponse(content={"message": "id does not exist or not found"}, status_code=404)
