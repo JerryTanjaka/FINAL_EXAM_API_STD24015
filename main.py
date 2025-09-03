967b0bf126f75bed763cd7934e06dd07a9f20c24
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-09-03 09:18:52 +0300

    feat : add post/phones route

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -13,6 +14,7 @@ class phones(BaseModel):
     brand: str
     model : str
     characteristics: Characteristics
+phones_mempory :List[phones] = []
 
 @app.get("/ping")
 def root():
@@ -21,3 +23,9 @@ def root():
 @app.get("/health")
 def health():
     return JSONResponse(content={"status": "ok"}, status_code=200)
+
+@app.post("/phones")
+def create_phone(list_phones: phones):
+    phones_mempory.extend(list_phones)
+    return phones_mempory
+
